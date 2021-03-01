33673f86b8bb4bb8b9a325923685fc121aa5eb93
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2021-03-01 08:06:15 -0600

    Committing where I stopped a few months ago.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const rust = import("./pkg");
+const canvas = document.getElementById("canvas");
 
 // Update this to use the time passed in by animationFrame. Target 30fps?
 // Use math from sandbox project to count frames.
@@ -8,30 +9,61 @@ const rust = import("./pkg");
 // Separate thread that does calculation outside of animation/render loop?
 
 rust.then(m => {
-    m.say_hello();
-
-    const FPS = 1000 / 30;
-    console.log(FPS);
     const client = new m.Client();
-    let initialTime = Date.now();
-    let frameCount = 0;
 
-    let otherTime = Date.now();
+    const fps = 30;
+    const interval = 1000 / fps;
+    console.log(interval);
+    let lastTime = 0;
+
+    // function render(time) {
+    //     requestAnimationFrame(render);
+    //
+    //     if (time - lastTime >= interval) {
+    //         // console.log(lastTime + "," + time);
+    //         client.render(time, lastTime);
+    //         lastTime = time;
+    //     }
+    // }
+
+    const FPS_THROTTLE = 1000.0 / 30.0;
+    const initialTime = Date.now();
+    let lastDrawTime = -1;
 
     function render() {
         window.requestAnimationFrame(render);
         const currentTime = Date.now();
 
-        frameCount++;
-        if (currentTime - initialTime >= FPS) {
-            if ((currentTime - otherTime) >= 1000) {
-                client.animate(frameCount);
-                otherTime = Date.now();
+        if (currentTime >= lastDrawTime + FPS_THROTTLE) {
+            client.render(currentTime, lastDrawTime);
+            lastDrawTime = currentTime;
+
+            if (window.innerHeight !== canvas.height || window.innerWidth !== canvas.width) {
+                canvas.height = window.innerHeight;
+                canvas.clientHeight = window.innerHeight;
+                // canvas.style.height = window.innerHeight.toString();
+
+                canvas.width = window.innerWidth;
+                canvas.clientWidth = window.innerWidth;
+                // canvas.style.width = window.innerWidth.toString();
             }
-            frameCount = 0;
-            initialTime = Date.now();
+
+            // let elapsedTime = currentTime - initialTime;
+            // client.update(elapsedTime, window.innerHeight, window.innerWidth);
         }
     }
+
+    document.addEventListener("keydown", event => {
+        if (event.key === "ArrowLeft") {
+            client.move_player_left();
+        } else if (event.key === "ArrowRight") {
+            client.move_player_right();
+        } else if (event.key === "ArrowDown") {
+            client.move_player_down()
+        } else if (event.key === "ArrowUp") {
+            client.move_player_up();
+        }
+    });
 
     render();
 }).catch(console.error);
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{window, CanvasRenderingContext2d, HtmlCanvasElement, ImageBitmap};
 
 #[wasm_bindgen]
 extern "C" {
@@ -7,25 +9,115 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn say_hello() {
-    log("Rust says fuck ysfafou");
-}
-
-#[wasm_bindgen]
 pub struct Client {
-    count: u64,
+    accumulated_time: f64,
+    frame_count: u8,
+    context: CanvasRenderingContext2d,
+    player: Player,
 }
 
 #[wasm_bindgen]
 impl Client {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
-        Self { count: 0 }
+        console_error_panic_hook::set_once();
+        let document = window().unwrap().document().unwrap();
+        let canvas = document.get_element_by_id("canvas").unwrap();
+        let canvas: HtmlCanvasElement = canvas
+            .dyn_into::<HtmlCanvasElement>()
+            .map_err(|_| ())
+            .unwrap();
+        let context = canvas
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<CanvasRenderingContext2d>()
+            .unwrap();
+
+        // ImageBitmap::
+        /*let callback = Closure::wrap(Box::new(move |event: KeyboardEvent| {
+            log(&format!("{}", event.key()));
+        }) as Box<dyn Fn(_)>);
+
+        window()
+            .unwrap()
+            .add_event_listener_with_callback("keydown", callback.as_ref().unchecked_ref())
+            .unwrap();
+        callback.forget();*/
+
+        Self {
+            accumulated_time: 0.,
+            frame_count: 0,
+            context,
+            player: Player::new(),
+        }
     }
 
     #[wasm_bindgen]
-    pub fn animate(&self, count: u8) {
-        // self.count += 1;
-        log(format!("{}", count).as_str());
+    pub fn render(&mut self, current_time: f64, last_time: f64) {
+        // log(&format!("{}", self.accumulated_time));
+        self.accumulated_time += current_time - last_time;
+        self.frame_count += 1;
+
+        if self.accumulated_time >= 1000.0 {
+            // log(&format!("frames: {}", self.frame_count));
+            self.frame_count = 0;
+            self.accumulated_time = 0.0;
+        }
+
+        self.context.set_fill_style(&JsValue::from("#fff")); // use .into()
+        self.context.fill_rect(0., 0., 480., 640.);
+        self.context.set_fill_style(&JsValue::from("#000"));
+        self.context
+            .fill_rect(self.player.x, self.player.y, 10.0, 10.0);
+
+        // self.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh
+    }
+
+    #[wasm_bindgen]
+    pub fn move_player_right(&mut self) {
+        self.player.move_right();
+    }
+
+    #[wasm_bindgen]
+    pub fn move_player_left(&mut self) {
+        self.player.move_left();
+    }
+
+    #[wasm_bindgen]
+    pub fn move_player_up(&mut self) {
+        self.player.move_up();
+    }
+
+    #[wasm_bindgen]
+    pub fn move_player_down(&mut self) {
+        self.player.move_down();
+    }
+}
+
+struct Player {
+    x: f64,
+    y: f64,
+}
+
+impl Player {
+    fn new() -> Self {
+        Self { x: 1., y: 1. }
+    }
+
+    fn move_right(&mut self) {
+        self.x += 1.;
+    }
+
+    fn move_left(&mut self) {
+        self.x -= 1.;
+    }
+
+    fn move_up(&mut self) {
+        self.y -= 1.;
+    }
+
+    fn move_down(&mut self) {
+        self.y += 1.;
     }
 }
